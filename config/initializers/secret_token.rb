a83a99c753988272ebf13edcf976a080f5ef7b44
Alexander Kabanov <shurik@encbox.com>
2011-01-26 21:14:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmnisocialExample::Application.config.secret_token = '1f18a62ddac53f31f8d960bf7ca493770771fe20204f8693c752b89adb6e36662e6721d229cb24351d2db241a55cbdd43d3f4481a7a47bf2f80f003cc6dc3d5e'
